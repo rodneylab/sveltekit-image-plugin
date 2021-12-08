8e285c645359b02a643d74c935c70fed8498ad62
Rodney Lab <codesign@rodneylab.com>
2021-12-08 10:11:52 +0000

    fix: 💫 update header link to home to prefetch

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -8,7 +8,7 @@
   >
   <nav class="nav">
     <ul>
-      <li><a href="/.">Home</a></li>
+      <li><a sveltekit:prefetch href="/.">Home</a></li>
       <li><a href="/contact">Contact</a></li>
     </ul>
   </nav>
