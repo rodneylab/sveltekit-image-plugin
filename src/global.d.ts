94102f4c0e268d8e315dc1996e78eb2755c6ea7a
Rodney Lab <codesign@rodneylab.com>
2022-04-12 12:16:40 +0100

    style: 🛁  linting

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="@sveltejs/kit" />
+
+import type { ILazyLoadInstance } from 'vanilla-lazyload';
+
+declare global {
+  interface Document {
+    lazyloadInstance: ILazyLoadInstance;
+  }
+
+  module '*.jpg?width=672';
+  module '*.jpg?width=1344;672;336&format=webp&srcset';
+  module '*.jpg?width=1344;672;336&srcset';
+}
